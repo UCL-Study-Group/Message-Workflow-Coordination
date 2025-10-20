cb408317b200aa969b4816493119c8896991d27f
TheSecondSoda <thomas.lind.spil@gmail.com>
2025-10-20 20:16:30 +0200

    initial commit

---
+++ Shared/WorkItem.cs
@@ -0,0 +1,9 @@
+﻿namespace Shared
+{
+    public class WorkItem
+    {
+        public int Id { get; set; }
+        public string Status { get; set; }
+        public string Data { get; set; }
+    }
+}
