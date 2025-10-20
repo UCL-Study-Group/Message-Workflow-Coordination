acf238cc7f42bacd489c2219b143074b823abf55
TheSecondSoda <thomas.lind.spil@gmail.com>
2025-10-20 20:19:20 +0200

    Added dependencies

--- RequestProject/WorkflowRequester.cs
+++ RequestProject/WorkflowRequester.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RabbitMQ.Client;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,11 @@ using System.Threading.Tasks;
 
 namespace RequestProject
 {
-    internal class WorkflowRequester
+    public class WorkflowRequester
     {
+        private readonly string _connectionString;
+        private readonly IConnection _rabbitConnection;
+        private readonly string _requestQueue = "work-requests";
+        private readonly string _replyQueue = "work-replies";
     }
 }
